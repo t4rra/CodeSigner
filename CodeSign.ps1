64716f5e94bf807c4303106db043867f68aa4a41
E-Soda <64815861+eaaasun@users.noreply.github.com>
2022-06-21 13:53:44 -0700

    add error messages

--- CodeSign.ps1
+++ CodeSign.ps1
@@ -2,36 +2,45 @@ param (
 	$operation='help',
 	$certName,
 	$folderPath,
-	$filter
+	$filter="*"
 )
 
 function error {
-	write-host "Error! Something you entered isn't correct. Please  " -ForegroundColor Red
-	write-host ".\CodeSign.ps1 [-operation] [Help/Sign/Create] [-certName] <Certificate Name> [-folderPath] <Folder Path>" -ForegroundColor Red
+	param (
+		$errorMSG='Something went wrong!'
+	)
+	write-host "ERROR! " -ForegroundColor Red -NoNewline
+	write-host $errorMSG -ForegroundColor Red
+	write-host ""
+	write-host "Please read the documentation by running this file without any parameters."
+	write-host ""
+	write-host "Syntax: .\CodeSign.ps1 -operation <[Sign]/[Create]> -certName <Certificate Name> -folderPath <Folder Path> -filter <filter>"
 	exit
 }
 
 function help {
-	start https://github.com/eaaasun/CodeSign
+	start https://github.com/eaaasun/CodeSigner
 }
 
 if ( $operation -eq 'sign') {
 	if ( $certName -eq $null -or $folderPath -eq $null ) {
-		error
+		error -errorMSG 'Missing parameters for script signing!'
 	}
 
 	$codeCertificate = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -eq "CN=$($certName)"}
 
 	if ( $codeCertificate -eq $null ) {
-		write-host "Certificate could not be found. Please create a certificate or check that your certificate name is spelled correctly."
-		exit
+		error -errorMSG 'Certificate not found! Create a certificate or check the spelling of the certificate.'
 	}
 	# https://stackoverflow.com/questions/13126175/get-full-path-of-the-files-in-powershell
-	$scripts=Get-ChildItem $folderPath -Filter *.ps1 | % { $_.FullName }
+	$scripts=Get-ChildItem $folderPath -Filter "$($filter).ps1" | % { $_.FullName }
 	foreach ( $script in $scripts ) {
 		Set-AuthenticodeSignature -FilePath $script -Certificate $codeCertificate -TimeStampServer http://timestamp.digicert.com
 	}
 } elseif ( $operation -eq 'create') {
+	if ( $certName -eq $null ) {
+		error -errorMSG 'Missing certificate name!'
+	}
 	# https://adamtheautomator.com/how-to-sign-powershell-script/
 	$authenticode = New-SelfSignedCertificate -Subject $certName -CertStoreLocation Cert:\LocalMachine\My -Type CodeSigningCert
 	$rootStore = [System.Security.Cryptography.X509Certificates.X509Store]::new("Root","LocalMachine")
@@ -44,5 +53,5 @@ if ( $operation -eq 'sign') {
 	$publisherStore.Close()
 
 } else {
-	start https://github.com/eaaasun/CodeSigner
+	help
 }
